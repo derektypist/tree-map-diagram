862ab8059b533c3a9bea3b2ffc348ccaade0f6cc
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-10-13 15:02:43 +0000

    Created drawTreeMap function at script.js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,34 @@ let movieData;
 let canvas = d3.select('#canvas');
 let tooltip = d3.select('#tooltip');
 
+// Draw Tree Map
+let drawTreeMap = () => {
+
+    // Set Up Hierarchy
+    let hierarchy = d3.hierarchy(movieData, (node) => {
+        return node['children'];
+    }).sum((node) => {
+        return node['value'];
+    }).sort((node1, node2) => {
+        return node2['value'] - node1['value'];
+    });
+
+    // Create Tree Map
+    let createTreeMap = d3.treemap().size([1000, 600]);
+    createTreeMap(hierarchy);
+    
+    // Set Up Movie Tiles
+    let movieTiles = hierarchy.leaves();
+    console.log(movieTiles);
+
+    // Set Up Block
+    let block = canvas.selectAll('g')
+        .data(movieTiles)
+        .enter()
+        .append('g')
+        .attr('transform', (movie) => {
+            return 'translate(' + movie['x0'] + ', ' + movie['y0'] + ')';
+        });
+
+
+};
