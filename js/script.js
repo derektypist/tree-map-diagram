8523f916d268b40af1ab55b07a167294ab3b55a7
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-10-13 14:49:17 +0000

    Added variables at script.js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Initialise and Define Variables
+let movieURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json';
+let movieData;
+let canvas = d3.select('#canvas');
+let tooltip = d3.select('#tooltip');
+
